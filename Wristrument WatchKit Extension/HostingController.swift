af5bbf8e0522ec8bfa8623c3eeb36088f6627d50
Marisa <contactmarisalu@gmail.com>
2019-10-19 21:55:27 -0700

    Initial Commit

---
+++ Wristrument WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Wristrument WatchKit Extension
+//
+//  Created by Marisa Lu on 10/19/19.
+//  Copyright © 2019 Marisa Lu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
